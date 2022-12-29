0690fb5466a5ae8c4f302fb0ec6c4e4fc48d5f4f
Caluril <mark.parr20@gmail.com>
2022-12-29 15:49:24 +1030

    Updates
    Updated Media to use a local Path to shorten the file locations. Looks neater and easier to maintain.

--- Modules/Media/Core.lua
+++ Modules/Media/Core.lua
@@ -1,57 +1,56 @@
-local LSM = LibStub("LibSharedMedia-3.0")
-local addonName, addonTable = ...
-
-if LSM == nil then return end
+local LSM = LibStub('LibSharedMedia-3.0')
+local AddOnName = ...
+local Path = [[Interface\AddOns\]]..AddOnName..[[Modules\Media\]]
 
 -- Backgrounds -- These can be either, .tga or .blp
--- LSM:Register("background", "my background's name", [[Interface\Addons\ElvUI_Ozzis_Media\Media\Background\mybackground.tga]])
+--LSM:Register("background", "my background's name", Path..[[Backgrounds\mybackground.tga]])
 
 -- Borders -- These can be either, .tga or .blp
--- LSM:Register("border", "my border's name", [[Interface\Addons\ElvUI_Ozzis_Media\Media\Border\myborder.tga]])
+--LSM:Register("border", "my border's name", Path..[[Borders\myborder.tga]])
 
 -- Icons -- These can be either, .tga or .blp
---LSM:Register("icon", "my icon's name", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Icons\myicon.blp]])
+--LSM:Register("icon", "my icon's name", Path..[[Icons\myicon.blp]])
 
 -- Statusbars
--- LSM:Register("statusbar", "my statusbar's name", [[Interface\Addons\ElvUI_Ozzis_Media\Statusbar\mytexture.tga]])
+--LSM:Register("statusbar", "my statusbar's name", Path..[[Statusbars\mytexture.tga]])
 
 -- Textures
--- LSM:Register("texture", "my texture's name", [[Interface\Addons\ElvUI_Ozzis_Media\Texture\mytexture.tga]])
+--LSM:Register("texture", "my texture's name", Path..[[Textures\mytexture.tga]])
 
 -- Fonts
-LSM:Register("font", "Calibri", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\calibri.ttf]])
-LSM:Register("font", "Calibri Bold", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\calibrib.ttf]])
-LSM:Register("font", "Calibri Bold Italic", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\calibriz.ttf]])
-LSM:Register("font", "Calibri Italic", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\calibrii.ttf]])
-LSM:Register("font", "Calibri Light", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\calibril.ttf]])
-LSM:Register("font", "Calibri Light Italic", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\calibrili.ttf]])
-LSM:Register("font", "Century Gothic", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\GOTHIC.ttf]])
-LSM:Register("font", "Century Gothic Italic", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\GOTHICI.ttf]])
-LSM:Register("font", "Cronos Pro Regular", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\cronospro-regular.otf]])
-LSM:Register("font", "Designosaur", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Designosaur.otf]])
-LSM:Register("font", "Designosaur Italic", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Designosaur-Italic.otf]])
-LSM:Register("font", "Eason Pro Regular", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Eason Pro Regular.otf]])
-LSM:Register("font", "Eason Pro Bold", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Eason Pro Bold.otf]])
-LSM:Register("font", "Enchanted Land", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Enchanted Land.otf]])
-LSM:Register("font", "Futura PT Book", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Futura-PT-Book.ttf]])
-LSM:Register("font", "Imagine", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\imagine.ttf]])
-LSM:Register("font", "Mark Pro", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Mark-Pro.otf]], LSM.LOCALE_BIT_western)
-LSM:Register("font", "Mark Pro Bold", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Mark-Pro-Bold.otf]], LSM.LOCALE_BIT_western)
-LSM:Register("font", "Mark Pro Medium", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\MarkPro-Medium.otf]], LSM.LOCALE_BIT_western)
-LSM:Register("font", "Mark Pro Regular", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\MarkPro-Regular.otf]])
-LSM:Register("font", "One Slice", [[Interface\Addons\ElvUI_Ozzis_Media\Media\Font\One Slice.otf]])
-LSM:Register("font", "PEPSI", [[Interface\Addons\ElvUI_Ozzis_Media\Media\Font\PEPSI.ttf]])
-LSM:Register("font", "Restiany", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\Restiany.otf]])
-LSM:Register("font", "Roberto Condensed Bold", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\RobotoCondensed-Bold.ttf]])
-LSM:Register("font", "ROG", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\ROG.otf]])
-LSM:Register("font", "Sanfransico Regular", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Font\SFUIText-Regular.otf]])
+LSM:Register("font", "Calibri", Path..[[Fonts\calibri.ttf]])
+LSM:Register("font", "Calibri Bold", Path..[[Fonts\calibrib.ttf]])
+LSM:Register("font", "Calibri Bold Italic", Path..[[Fonts\calibriz.ttf]])
+LSM:Register("font", "Calibri Italic", Path..[[Fonts\calibrii.ttf]])
+LSM:Register("font", "Calibri Light", Path..[[Fonts\calibril.ttf]])
+LSM:Register("font", "Calibri Light Italic", Path..[[Fonts\calibrili.ttf]])
+LSM:Register("font", "Century Gothic", Path..[[Fonts\GOTHIC.ttf]])
+LSM:Register("font", "Century Gothic Italic", Path..[[Fonts\GOTHICI.ttf]])
+LSM:Register("font", "Cronos Pro Regular", Path..[[Fonts\cronospro-regular.otf]])
+LSM:Register("font", "Designosaur", Path..[[Fonts\Designosaur.otf]])
+LSM:Register("font", "Designosaur Italic", Path..[[Fonts\Designosaur-Italic.otf]])
+LSM:Register("font", "Eason Pro Regular", Path..[[Fonts\Eason Pro Regular.otf]])
+LSM:Register("font", "Eason Pro Bold", Path..[[Fonts\Eason Pro Bold.otf]])
+LSM:Register("font", "Enchanted Land", Path..[[Fonts\Enchanted Land.otf]])
+LSM:Register("font", "Futura PT Book", Path..[[Fonts\Futura-PT-Book.ttf]])
+LSM:Register("font", "Imagine", Path..[[Fonts\imagine.ttf]])
+LSM:Register("font", "Mark Pro", Path..[[Fonts\Mark-Pro.otf]], LSM.LOCALE_BIT_western)
+LSM:Register("font", "Mark Pro Bold", Path..[[Fonts\Mark-Pro-Bold.otf]], LSM.LOCALE_BIT_western)
+LSM:Register("font", "Mark Pro Medium", Path..[[Fonts\MarkPro-Medium.otf]], LSM.LOCALE_BIT_western)
+LSM:Register("font", "Mark Pro Regular", Path..[[Fonts\MarkPro-Regular.otf]])
+LSM:Register("font", "One Slice", Path..[[Fonts\One Slice.otf]])
+LSM:Register("font", "PEPSI", Path..[[Fonts\PEPSI.ttf]])
+LSM:Register("font", "Restiany", Path..[[Fonts\Restiany.otf]])
+LSM:Register("font", "Roberto Condensed Bold", Path..[[Fonts\RobotoCondensed-Bold.ttf]])
+LSM:Register("font", "ROG", Path..[[Fonts\ROG.otf]])
+LSM:Register("font", "Sanfransico Regular", Path..[[Fonts\SFUIText-Regular.otf]])
 
 -- Sounds
-LSM:Register("sound", "Guild Wars 2 Message", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Sound\GW2Message.ogg]])
-LSM:Register("sound", "Guild Wars 2 Mail", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Sound\GW2Email.ogg]])
-LSM:Register("sound", "Carbonite", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Sound\QuestComplete.ogg]])
-LSM:Register("sound", "Map Ping", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Sound\MapPing.ogg]])
-LSM:Register("sound", "Oh Shit!", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Sound\oh-shit.ogg]])
-LSM:Register("sound", "Loot Coin", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Sound\LootCoinSmall.ogg]])
-LSM:Register("sound", "Ion Skill Issue", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Sound\frankly.ogg]])
-LSM:Register("sound", "iPhone Ding", [[Interface\AddOns\ElvUI_Ozzis_Media\Media\Sound\iphone-ding-sound.ogg]])
+LSM:Register("sound", "Guild Wars 2 Message", Path..[[Sounds\GW2Message.ogg]])
+LSM:Register("sound", "Guild Wars 2 Mail", Path..[[Sounds\GW2Email.ogg]])
+LSM:Register("sound", "Carbonite", Path..[[Sounds\QuestComplete.ogg]])
+LSM:Register("sound", "Map Ping", Path..[[Sounds\MapPing.ogg]])
+LSM:Register("sound", "Oh Shit!", Path..[[Sounds\oh-shit.ogg]])
+LSM:Register("sound", "Loot Coin", Path..[[Sounds\LootCoinSmall.ogg]])
+LSM:Register("sound", "Ion Skill Issue", Path..[[Sounds\frankly.ogg]])
+LSM:Register("sound", "iPhone Ding", Path..[[Sounds\iphone-ding-sound.ogg]])
